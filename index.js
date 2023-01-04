a6fe25f0ffd703876fc4452ad0217633f6017274
Usiel-03A7 <shebastyanmail.com>
2023-01-03 19:12:09 -0600

    start the course node whit the starter programation

---
+++ index.js
@@ -0,0 +1,7 @@
+const names = ['Usiel', 'Pedro', 'Alberto', 'Juan'];
+const namesHi = names.map(function (name) {
+  console.log('Hola', name);
+  console.log('-------------');
+  console.log(`hola ${name}`);
+}
+);
