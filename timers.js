50fa8a71545650e03c88f425e4121918b8adcf2e
Usiel-03A7 <shebastyanmail.com>
2023-01-03 19:22:43 -0600

    learn timers and use one by one

---
+++ timers.js
@@ -0,0 +1,12 @@
+// setInterval //ejkecuta un código cada cierto tiempo 
+
+// setInterval(()=>{
+//     console.log('Hola mundo');
+// } ,1000)
+
+
+// setTimeout  // ejecuta el códiogo despues de cierto tiempo   
+
+setTimeout(() => {
+    console.log('Hola mundo');
+}, 5000)
