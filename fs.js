a22be744f44211302a6161e931b858763f149ede
Usiel-03A7 <shebastyanmail.com>
2023-01-07 10:11:20 -0600

    comment use from fs

--- fs.js
+++ fs.js
@@ -4,6 +4,7 @@
  */
 const fs = require('fs');
 
+// manera normal
 // const texto = fs.readFileSync('./data/first.txt','utf-8')
 // const texto2 = fs.readFileSync('./data/second.txt')
 
@@ -11,6 +12,8 @@ const fs = require('fs');
 //  console.log(texto2.toString());
 
 //  fs.writeFileSync('./data/pedro.css','body{ background:#000;} ');
+
+//manera asincrona <= 
 fs.readFile('./data/first.txt', (err,data) => {
     if(err){
         console.log('Hay un error'); 
