27dfbfea758627b67785c79d685936c6545a958d
Usiel-03A7 <shebastyanmail.com>
2023-01-07 11:28:03 -0600

    use http module

--- http.js
+++ http.js
@@ -1,9 +1,22 @@
 const { log } = require('console');
 const http = require('http')
 
-http.createServer( (request, response) => {
-    response.write('Hello world')
+http.createServer((request, response) => {
 
+    if (request.url === '/usiel') {
+        response.write('Usiel online');
+        return response.end();
+    }
+    if (request.url === '/') {
+        response.write('Estas en home');
+       return response.end();
+    }
+    response.write(`
+    <h1> no found page</h1>
+    <p>this page is not found</p>
+    <a href='/' > back home</a>
+    `);
     response.end();
+
 }).listen(3000)
 console.log('servidor escuchando en el puerto 3000');
